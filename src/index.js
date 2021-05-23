6eb9a9c7277f1ef6da46a9b5d595ae8349bbf676
jengpark <75283856+jengpark@users.noreply.github.com>
2021-05-23 16:33:11 -0700

    Added weather icon and cleanup
    refined unit conversions, added weather icon, changed weather description and layout of weather details for a cleaner appearance, removed unused images

--- src/index.js
+++ src/index.js
@@ -15,36 +15,30 @@ let minutes = ('0' + now.getMinutes()).slice(-2);
 let dayTime = document.querySelector("#day-time");
 dayTime.innerHTML = `${day} ${hours}:${minutes}`;
 
-function convertCelsiusLink(event) {
-  event.preventDefault();
-  let celsiusTemp = document.querySelector("#current-temp");
+function temperatureFahrenheitLink(response) {
   let currentTemp = document.querySelector("#current-temp");
-  let tempConversion = currentTemp.innerHTML;
-  celsiusTemp.innerHTML = `${Math.round(((tempConversion - 32) * 5) / 9)}°C`;
-}
-let celsiusLink = document.querySelector("#celsiusLink");
-celsiusLink.addEventListener("click", convertCelsiusLink);
-
-function temperaturefahrenheitLink(response) {
-  let currentTemp = Math.round(response.data.main.temp);
-  let fahrenheitTemp = document.querySelector("#current-temp");
   let feelsLike = document.querySelector("#feels-like");
   let humidity = document.querySelector("#humidity");
   let windspeed = document.querySelector("#windspeed");
   let description = document.querySelector("#weather-description");
+  let icon = document.querySelector("#weather-icon");
   document.querySelector("#cityName").innerHTML = response.data.name;
-  fahrenheitTemp.innerHTML = currentTemp;
+
+  fahrenheitTemp = response.data.main.temp;
+
+  currentTemp.innerHTML = Math.round(fahrenheitTemp);
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   humidity.innerHTML = response.data.main.humidity;
   windspeed.innerHTML = Math.round(response.data.wind.speed);
-  description.innerHTML = response.data.weather[0].main;
+  description.innerHTML = response.data.weather[0].description;
+  icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 function searchCity(city) {
   let apiKey = "0523df1bb58e895c347086de47bedb0f";
   let unit = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(temperaturefahrenheitLink);
+  axios.get(apiUrl).then(temperatureFahrenheitLink);
 }
 
 function searchButton(event) {
@@ -53,25 +47,34 @@ function searchButton(event) {
   searchCity(city);
 }
 
-function getCoordinates(position) {
-  let apiKey = "0523df1bb58e895c347086de47bedb0f";
-  let unit = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(temperaturefahrenheitLink);
+function convertCelsiusLink(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#current-temp");
+
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemp = ((fahrenheitTemp - 32) * 5) / 9;
+  currentTemp.innerHTML = Math.round(celsiusTemp);
 }
-navigator.geolocation.getCurrentPosition(getCoordinates);
 
+function convertfahrenheitLink(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#current-temp");
 
-function getCurrentLocation(event) {
-    event.preventDefault();
-    navigator.geolocation.getCurrentPosition(getCoordinates);
-  }
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  currentTemp.innerHTML = Math.round(fahrenheitTemp);
+}
 
+let fahrenheitTemp = null;
 
 let form = document.querySelector("form");
 form.addEventListener("submit", searchButton);
 
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
 searchCity("San Diego");
+
+let celsiusLink = document.querySelector("#celsiusLink");
+celsiusLink.addEventListener("click", convertCelsiusLink);
+
+let fahrenheitLink = document.querySelector("#fahrenheitLink");
+fahrenheitLink.addEventListener("click", convertfahrenheitLink);
